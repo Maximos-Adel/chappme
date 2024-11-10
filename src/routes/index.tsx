0a66d98a197837d7951b2931da9078ac9f42b4dd
Maximos-Adel <maximosadel@gmail.com>
2024-11-11 01:37:16 +0200

    first commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,10 @@ import { Navigate, createBrowserRouter } from 'react-router-dom';
 import NoonLayout from '../theme/layouts/NoonLayout';
 import Checkout from '../views/Checkout';
 import Verification from '../views/Verification';
-// import MainLayout from '../theme/layouts/MainLayout';
-// import FabLayout from '../theme/layouts/FabLayout';
+
 import FabHome from '../views/FabHome';
 import FabPasscode from '../views/FabPasscode';
 import NotFound from '../views/NotFound';
-// import FabLayout from "../theme/layouts/FabLayout";
 
 export const routesList = [
   {
